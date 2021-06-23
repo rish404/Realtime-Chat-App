e0759a936cabc3f573319d64d056442665918f22
rish404 <rish.destiny@gmail.com>
2021-06-24 01:08:06 +0530

    added user disconnect functionality

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const users = {}
 
 io.on('connection', socket => {
     socket.on('new-user', name => {
+      users[saocket.id] = name
+      socket.broadcast.emit('user-connected', name)
+    })
 
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
-    })
-    socket.emit('chat-message', 'Hello World')
     socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+      })
+      socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+      })
     })
-})
